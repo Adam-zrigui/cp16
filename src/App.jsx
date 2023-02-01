589eaf0aa1cebe2260b7e7851e0cc70a16f4dc07
adzrs <101660575+opak-exe@users.noreply.github.com>
2023-02-01 18:05:20 +0100

    google cloud not free

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Map from './Map'
+function App() {
+
+
+  return (
+    <div className="App">
+      <div>
+<Map />
+</div>     
+    </div>
+  )
+}
+
+export default App
